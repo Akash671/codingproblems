718534d32c038e0900bd073d32f6f31c93836b2c
Akash671 <akashsaini454545gmail.com>
2021-04-14 12:06:52 +0530

    Fit Squares in Triangle

---
+++ codechef/Fit_Squares_in_Triangle.cpp
@@ -0,0 +1,25 @@
+/* auhtor : @akash kumar */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int n)
+{
+	n=n-2;
+	n=floor(n/2);
+	return n*(n+1)/2;
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		cout<<solve(n)<<endl;
+	}
+	return 0;
+}
+
